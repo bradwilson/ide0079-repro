4af3200dae461ee8d2aac463c0205c238c45f42a
Brad Wilson <dotnetguy@gmail.com>
2024-11-26 12:52:21 -0800

    Initial revision

---
+++ Example.cs
@@ -0,0 +1,10 @@
+﻿using Xunit;
+
+public class Example
+{
+    [Fact]
+#pragma warning disable xUnit1001 // Fact methods cannot have parameters
+    public void Test1(int _)
+#pragma warning restore xUnit1001 // Fact methods cannot have parameters
+    { }
+}
